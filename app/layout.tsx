b29d5ae1cf157e74012cdd77298c7cd0270eeabf
Mattias Viola <mattiasviola@MacBook-Air-de-Mattias.local>
2026-08-28 10:04:41 +0200

    Add Google Ads tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const manrope = Manrope({
@@ -20,6 +21,18 @@ export default function RootLayout({
     <html lang="fr">
       <body className={manrope.className}>
         {children}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18412017925"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-18412017925');
+          `}
+        </Script>
       </body>
     </html>
   );
